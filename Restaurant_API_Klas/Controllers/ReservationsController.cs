2ba054e59a2825ee4ef9afaf6a62bccc131d7922
Kenan Kurda <intecbrussel24@gmail.com>
2024-06-20 12:06:26 +0200

    Add project files.

---
+++ Restaurant_API_Klas/Controllers/ReservationsController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Restaurant_API_Klas.Data;
+using Restaurant_API_Klas.Models.DTOs;
+using Restaurant_API_Klas.Extensions;
+
+namespace Restaurant_API_Klas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservationsController : ControllerBase
+    {
+        private readonly RestaurantContext _context;
+        public ReservationsController(RestaurantContext context)
+        {
+                _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ReservationDetailsDto>>> GetReservations()
+        {
+            var reservations = await _context.Reservations
+                .Include(r => r.Customer)
+                .Include(r => r.Table)
+                .ToListAsync(); 
+            var reservationsDto = reservations.Select(r => r.ToReservationDetailsDto());
+
+            //return Ok(reservations.AsQueryable().ToReservationDetailsDto());
+
+            return Ok(reservationsDto);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ReservationDetailsDto>> GetReservation(int id)
+        {
+            var reservation = await _context.Reservations
+                .Include(r => r.Customer)
+                .Include(r => r.Table)
+                .FirstOrDefaultAsync(r => r.ReservationId == id);
+
+            if (reservation == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(reservation.ToReservationDetailsDto());
+        }
+
+    }
+}
---
+++ Restaurant_API_Klas/Extensions/ReservationMappingExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Restaurant_API_Klas.Models;
+
+using Restaurant_API_Klas.Models.DTOs;
+
+namespace Restaurant_API_Klas.Extensions
+{
+    public static class ReservationMappingExtensions
+    {
+        public static ReservationDetailsDto ToReservationDetailsDto(this Reservation reservation)
+        {
+            return new ReservationDetailsDto
+            {
+                ReservationId = reservation.ReservationId,
+                CustomerId = reservation.CustomerId,
+                CustomerName = reservation.Customer.Name,
+                TableId = reservation.TableId,
+                DateTime = reservation.DateTime,
+                SpecialRequests = reservation.SpecialRequests
+            };
+
+        }
+    }
+}
